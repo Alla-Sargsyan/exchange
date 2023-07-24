6a6722033fb08f77ff683505fd6bad7c5bc109e7
Alvard <allasargsyan1111@gmail.com>
2023-07-24 18:02:30 +0400

    created AMD to USD exchange rate calculator

---
+++ js/index.js
@@ -0,0 +1,56 @@
+const amd = document.querySelector("#amd");
+const usd = document.querySelector("#usd");
+
+amd.addEventListener("input", () =>{
+    const request = new XMLHttpRequest(); // ստեղծեցինք հարցում
+
+    setTimeout(() =>{
+        if(typeof(amd.value) !== "number" && isNaN(amd.value)){
+            usd.value = "";
+            amd.value = "";
+        }
+    }, 10);
+
+    //նշեցինք հարցման մեթոդն ու ճանապարհը
+    request.open("GET","js/exchange.json");
+
+    // նշեցինք հարցման վերնագրերը
+    request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+
+    //ուղարկեցինք հարցումը
+    request.send();
+
+    request.addEventListener("readystatechange",() =>{
+        if (request.readyState === 4 && request.status === 200 ) {
+            const data = JSON.parse(request.response);
+            usd.value = (parseFloat(amd.value) / parseFloat(data.current.usd)).toFixed(2);
+        } else{
+            usd.value = "Something went wrong";
+        }
+    });
+});
+
+usd.addEventListener("input", () =>{
+    const request = new XMLHttpRequest(); 
+
+    setTimeout(() =>{
+        if(typeof(usd.value) !== "number" && isNaN(usd.value)){
+            usd.value = "";
+            amd.value = "";
+        }
+    }, 10);
+
+
+    request.open("GET","js/exchange.json");
+    request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+    request.send();
+    request.addEventListener("readystatechange",() =>{
+        if (request.readyState === 4 && request.status === 200 ) {
+            const data = JSON.parse(request.response);
+            amd.value = (parseFloat(usd.value) * parseFloat(data.current.usd)).toFixed(2);
+        } else{
+            amd.value = "Something went wrong";
+        }
+    });
+});
+
